891f26c44662d47671d8b10fe07d93253e359097
noel.diril <noeldiril@MacBook-Air-de-Noel.local>
2017-12-16 17:35:01 +0100

    TP-9 10

---
+++ tpGit/console/src/main/java/Console.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import org.apache.log4j.Logger;
+
+public class Console {
+    private final static Logger logger = Logger.getLogger(Console.class.getName());
+
+    public static void main(String[] args) {
+        final Logger logger = Logger.getLogger(Console.class);
+        
+        int X = 5;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("X personnalisé ? y/n >> ");
+        String choice = sc.nextLine();
+        
+        if(choice.equals("y")) {
+            System.out.print("Entrez X : ");
+            X = sc.nextInt();
+        }
+        
+        logger.info("X vaut : " + X);
+        
+        for(int i=0; i<X; i++) {
+            logger.info("Valeur fib " + Fibonacci.fib(i));
+        }
+    }
+}
